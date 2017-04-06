3b220451b3c07f7672857b3f6be669942c46a498
dimitrovmaksim <dimitrov.maksim@gmail.com>
2017-04-07 01:02:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 (function(){
 
   let app = angular.module('blogApp', []);
-  app.config(['$qProvider', function ($qProvider) {
-    $qProvider.errorOnUnhandledRejections(false);
-  }]);
+
   app.controller('BlogController', [ '$http', function($http){
     let self = this;
     self.articles = [];
